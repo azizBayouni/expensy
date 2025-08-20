2ace68cc72025ae70c965d22602c22a9d6db34f2
Aziz Bayouni <almssk0@gmail.com>
2025-08-20 09:57:14 +0000

    match the categories colors in these 2 cards for each category

--- src/components/charts/categories-donut-chart.tsx
+++ src/components/charts/categories-donut-chart.tsx
@@ -21,8 +21,8 @@ interface CategoriesDonutChartProps {
 }
 
 const COLORS = [
-  'hsl(var(--chart-2))',
   'hsl(var(--chart-1))',
+  'hsl(var(--chart-2))',
   'hsl(var(--chart-3))',
   'hsl(var(--chart-4))',
   'hsl(var(--chart-5))',
